c55cccddd99ddbe113b7faf501b0265a4bd962ea
Kevin Earls <kevin@kevinearls.com>
2015-07-01 11:26:02 +0200

    Updated to 6.2.1, fixed some typos

--- oseFuseCartridgeSmokeTest.sh
+++ oseFuseCartridgeSmokeTest.sh
@@ -1,23 +1,26 @@
 #
+set -x 
 export OSE_FUSE_CARTRIDGE_RPM_URL=$1
 export OSE_FUSE_CARTRIDGE_RPM=`echo ${OSE_FUSE_CARTRIDGE_RPM_URL} | sed 's/^.*\///g'`
+echo "OSE_FUSE_CARTRIDGE_RPM " ${OSE_FUSE_CARTRIDGE_RPM}
 export APP_NAME=smoketestapp
 export SMOKETESTNAMESPACE=smokenamespace
 rhc setup --rhlogin demo --password openshift --create-token --server vm.openshift.example.com
 rhc app-delete --confirm ${APP_NAME}
 rm -rf ${APP_NAME}
-sudo oo-admin-ctl-cartridge --command delete --name fuse-6.2.0
+sudo oo-admin-ctl-cartridge --command delete --name fuse-6.2.1
 sudo yum remove --assumeyes openshift-origin-cartridge-fuse
 rm -rf *.rpm
-wget OSE_FUSE_CARTRIDGE_RPM_URL
-sudo yum --assumeyes localinstall OSE_FUSE_CARTRIDGE_RPM
+set -e
+wget ${OSE_FUSE_CARTRIDGE_RPM_URL}
+sudo yum --assumeyes localinstall ${OSE_FUSE_CARTRIDGE_RPM}
 sudo oo-admin-ctl-cartridge --command import-profile --activate
 sudo oo-admin-ctl-cartridge --command list 
 
 #
 # Create the smoketest app
 # 
-rhc app-create ${APP_NAME} fuse-6.2.0 --namespace ${SMOKETESTNAMESPACE}
+rhc app-create ${APP_NAME} fuse-6.2.1 --namespace ${SMOKETESTNAMESPACE}
 rhc apps
 rhc show-app ${APP_NAME}
 
