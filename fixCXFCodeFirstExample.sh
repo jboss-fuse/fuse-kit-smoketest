526ba1dc5594a6e34ffa923a0b3fd369c55fb9f8
Kevin Earls <kevin@kevinearls.com>
2016-02-29 10:41:47 +0100

    Update so this can run on a mac

--- fixCXFCodeFirstExample.sh
+++ fixCXFCodeFirstExample.sh
@@ -1,4 +1,9 @@
 #
 #
 #
-sed -i 's/address=\"\/order\/"/address=\"\/orderCode\/"/g' ${FUSE_HOME}/quickstarts/cxf/camel-cxf-code-first/src/main/resources/OSGI-INF/blueprint/camel-route.xml
+if [ "$(uname)" == "Darwin" ]; then
+    mv ${FUSE_HOME}/quickstarts/cxf/camel-cxf-code-first/src/main/resources/OSGI-INF/blueprint/camel-route.xml ${FUSE_HOME}/quickstarts/cxf/camel-cxf-code-first/src/main/resources/OSGI-INF/blueprint/camel-route-orig.xml
+    sed 's/address=\"\/order\/"/address=\"\/orderCode\/"/g'  /Users/kearls/fuse/jboss-fuse-6.3.0.redhat-025/quickstarts/cxf/camel-cxf-code-first/src/main/resources/OSGI-INF/blueprint/camel-route-orig.xml > ${FUSE_HOME}/quickstarts/cxf/camel-cxf-code-first/src/main/resources/OSGI-INF/blueprint/camel-route.xml
+else
+   sed -i 's/address=\"\/order\/"/address=\"\/orderCode\/"/g' ${FUSE_HOME}/quickstarts/cxf/camel-cxf-code-first/src/main/resources/OSGI-INF/blueprint/camel-route.xml
+fi
