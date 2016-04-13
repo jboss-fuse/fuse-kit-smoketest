673afe9858fcd8e6aff2be1bba2aeba986ad8956
Kevin Earls <kevin@kevinearls.com>
2016-04-13 10:48:40 +0200

    Extracted os dependent code

--- fuseSmokeTest.groovy
+++ fuseSmokeTest.groovy
@@ -1,119 +1,147 @@
-
-
-
-    stage 'define tools'
+stage 'define tools'
 // Get the tools and override EVs which may be set on the node
-    def M2_HOME = tool 'maven-3.2.3'  // TODO fix 3.3.3 on windows node
-    def JAVA_HOME = tool 'jdk8'
-    env.JAVA_HOME="${JAVA_HOME}"
-    env.M2_HOME="${M2_HOME}"
-    env.PATH = "${M2_HOME}/bin:${JAVA_HOME}/bin:${env.PATH}"
+def M2_HOME = tool 'maven-3.2.3'  // TODO fix 3.3.3 on windows node
+def JAVA_HOME = tool 'jdk8'
+env.JAVA_HOME = "${JAVA_HOME}"
+env.M2_HOME = "${M2_HOME}"
+env.PATH = "${M2_HOME}/bin:${JAVA_HOME}/bin:${env.PATH}"
 
 // Get the zipfile name and home directory from the full download URL
-    env.FUSE_KIT_URL = "${FUSE_KIT_URL}"
+env.FUSE_KIT_URL = "${FUSE_KIT_URL}"
 
-    def lastSlash = FUSE_KIT_URL.lastIndexOf("/");
-    def zipFileName = FUSE_KIT_URL.substring(lastSlash + 1, FUSE_KIT_URL.length());
-    def temp = zipFileName.substring(0, zipFileName.length() - 4); // strip off .zip
-    def version = temp.substring("jboss-fuse-full-".size());
-    def fuseHome = "jboss-fuse-" + version
+def lastSlash = FUSE_KIT_URL.lastIndexOf("/");
+def zipFileName = FUSE_KIT_URL.substring(lastSlash + 1, FUSE_KIT_URL.length());
+def temp = zipFileName.substring(0, zipFileName.length() - 4); // strip off .zip
+def version = temp.substring("jboss-fuse-full-".size());
+def fuseHome = "jboss-fuse-" + version
 
-    env.ZIPFILENAME="${zipFileName}"
-    env.FUSE_HOME="${fuseHome}"
-    env.VERSION="${version}"
+// TODO doI need all of these?
+env.ZIPFILENAME = "${zipFileName}"
+env.FUSE_HOME = "${fuseHome}"
+env.VERSION = "${version}"
 //env.WORKSPACE="${PWD}"    // FIXME do I need this?
 
-    def unix = isUnix()
+stage 'cleanup from previous runs'
+if (isUnix()) {
+    sh 'pkill -f org.apache.karaf.main.Main || true'  // TODO can we do something similar on windows?
+}
+cleanup("jboss-fuse*")
 
-    stage 'cleanup from previous runs'
-    if (unix) {
-        sh 'pkill -f org.apache.karaf.main.Main || true'
-        sh 'rm -rf jboss-fuse*'
-        sh 'env | sort'
+stage 'download kit'
+downloadAndUnzipKit(FUSE_KIT_URL, zipFileName)
+uncommentAdminUserPassword(fuseHome)
+
+sh 'cat ' + fuseHome + '/etc/user.properties'
+
+try {
+    // Start the broker
+    stage 'starting broker'
+    startBroker(fuseHome)
+    sleep 120
+
+    // Build and deploy the quickstarts
+    stage 'Build and Deploy Quickstarts'
+    maven('--version')
+    echo '>>>> maven --file ${fuseHome}/quickstarts/pom.xml '
+    maven("--file ${fuseHome}/quickstarts/pom.xml clean install")
+
+    stage 'deploy quickstarts'
+    // FIXME we need a windows version
+    if (isUnix()) {
+        sh './deployQuickStarts.sh'
     } else {
-        bat 'rm -rf jboss-fuse*'
+        // TODO
     }
 
-// Download the kit and unzip it
-    stage 'download'
-    if(unix) {
-        sh 'wget --no-verbose ${FUSE_KIT_URL}'
-        sh 'unzip -q ${ZIPFILENAME}'
+    stage 'Quickstart tests'
+    // TODO how do deal with ${PWD} here
+    maven('-DFUSE_HOME=${PWD}/${FUSE_HOME} -Dsurefire.rerunFailingTestsCount=2 -Pquickstarts clean test')
+
+    /// TODO we need a client command method
+    stage 'Create a fabric'
+    executeClientCommand(fuseHome, '-u admin -p admin "fabric:create --wait-for-provisioning"')
+    /*
+    if (isUnix()) {
+        sh '${FUSE_HOME}/bin/client -u admin -p admin "fabric:create --wait-for-provisioning"'
     } else {
-        bat 'wget --no-verbose --no-check-certificate %FUSE_KIT_URL%'
-        bat 'unzip -q %ZIPFILENAME%'
-    }
+        // FIXME
+    }*/
 
-// 3. Uncomment admin user in etc/user.properties
-    if (unix) {
-        sh 'sed -i \'s/^#admin/admin/g\' ${FUSE_HOME}/etc/users.properties'
+    stage 'Other tests'
+    maven('-DFUSE_HOME=${PWD}/${FUSE_HOME} -Dsurefire.rerunFailingTestsCount=2 -Pnoquickstarts clean test')
+} finally {
+    stage 'Final shutdown'
+    stopBroker(fuseHome)
+
+
+    echo "Shutdown complete"
+    stage 'shutdown complete'
+    // FIXME!!!! step([$class: 'JUnitResultArchiver', testDataPublishers: [[$class: 'JUnitFlakyTestDataPublisher']], testResults: '**/target/*-reports/*.xml'])
+
+
+    if (!unix) {
+        build job: 'Reboot_windows', quietPeriod: 30, wait: false
     } else {
-        bat 'sed -i \'s/^#admin/admin/g\' %FUSE_HOME%/etc/users.properties'
+        //stage 'clear out workspace'
+        //deleteDir()  //Looks like we can't do this on windows  FIXME on unix do this here or in caller
     }
+}
 
-    try {
-        // 4. Start the broker
-        stage 'starting broker'
-        if (unix) {
-            sh './${FUSE_HOME}/bin/start'
-        } else {
-            bat '%FUSE_HOME%\\bin\\start'
-        }
-        sleep 120
-
-        // Build and deploy the quickstarts
-        stage 'Build and Deploy Quickstarts'
-        if (unix) {
-            //sh 'cd ${FUSE_HOME}/quickstarts'
-            sh 'pwd'
-            sh 'ls'
-            sh 'mvn --version'
-            sh 'mvn --file ${FUSE_HOME}/quickstarts/pom.xml clean install'
-
-            echo "Deploying quickstarts"
-            sh './deployQuickStarts.sh'
-        } else {
-            // TODO
-        }
-
-        stage 'Quickstart tests'
-        if (unix) {
-            sh 'mvn -DFUSE_HOME=${PWD}/${FUSE_HOME} -Dsurefire.rerunFailingTestsCount=2 -Pquickstarts clean test'
-        } else {
-            // TODO
-        }
-
-        stage 'Create a fabric'
-        if (unix) {
-            sh '${FUSE_HOME}/bin/client -u admin -p admin "fabric:create --wait-for-provisioning"'
-        } else {
-            // FIXME
-        }
-
-        stage 'Other tests'
-        if (unix) {
-            sh 'mvn -DFUSE_HOME=${PWD}/${FUSE_HOME} -Dsurefire.rerunFailingTestsCount=2 -Pnoquickstarts clean test'
-        } else {
-            // TODO
-        }
-
-    } finally {
-        stage 'Final shutdown'
-        if (unix) {
-            sh './${FUSE_HOME}/bin/stop'
-        } else {
-            bat '%FUSE_HOME%\\bin\\stop'
-        }
-
-        echo "Shutdown complete"
-        stage 'shutdown complete'
-        // FIXME!!!! step([$class: 'JUnitResultArchiver', testDataPublishers: [[$class: 'JUnitFlakyTestDataPublisher']], testResults: '**/target/*-reports/*.xml'])
-
-
-        if (!unix) {
-            build job: 'Reboot_windows', quietPeriod: 30, wait: false
-        } else {
-            //stage 'clear out workspace'
-            //deleteDir()  //Looks like we can't do this on windows
-        }
+// TODO find somewhere to put this code so it can be shared.
+def cleanup(directoryName) {
+    if (isUnix()) {
+        sh 'rm -rf ' + directoryName
+    } else {
+        bat 'rm -rf ' + directoryName
     }
+}
+
+def downloadAndUnzipKit(downloadUrl, zipFileName) {
+    if (isUnix()) {
+        sh 'wget --no-verbose ' + downloadUrl
+        sh 'unzip -q ' + zipFileName
+    } else {
+        bat 'wget --no-verbose --no-check-certificate ' + downloadUrl
+        bat 'unzip -q ' + zipFileName
+    }
+}
+
+def uncommentAdminUserPassword(fuseHomeDirectory) {
+    if (isUnix()) {
+        sh 'sed -i \'s/^#admin/admin/g\' ' + fuseHomeDirectory + '/etc/users.properties'
+    } else {
+        bat 'sed -i \'s/^#admin/admin/g\' ' + fuseHomeDirectory + '/etc/users.properties'
+    }
+}
+
+def startBroker(fuseHomeDirectory) {
+    if (isUnix()) {
+        sh './' + fuseHomeDirectory + '/bin/start'
+    } else {
+        bat fuseHomeDirectory + '\\bin\\start'
+    }
+}
+
+def stopBroker(fuseHomeDirectory) {
+    if (isUnix()) {
+        sh './' + fuseHomeDirectory + '/bin/stop'
+    } else {
+        bat fuseHomeDirectory + '\\bin\\stop'
+    }
+}
+
+def executeClientCommand(fuseHomeDirectory, command) {
+    if (isUnix()) {
+        sh './' + fuseHomeDirectory + '/bin/client ' + command
+    } else {
+        bat fuseHomeDirectory + '\\bin\\client '
+    }
+}
+
+def maven(command) {
+    if (isUnix()) {
+        sh 'mvn ' + command
+    } else {
+        bat 'mvn ' + command
+    }
+}
